b3a17c4cdce865a87a8aedcc13a8fdf207e752b4
bucard <rafaelbucard@gmail.com>
2021-04-25 22:14:32 -0300

    Create 2021_04_26_000843_criar_tabela_series.php

---
+++ series/database/migrations/2021_04_26_000843_criar_tabela_series.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CriarTabelaSeries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('series', function(Blueprint $table){
+            $table->string('nome'); 
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('series');
+        
+    }
+}
