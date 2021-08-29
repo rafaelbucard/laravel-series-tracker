fecea0c94bf72c354db43f3cf772b8412fac3510
rafaelbucard <rafaelbucard@gmail.com>
2021-08-29 20:47:10 -0300

    Composer

--- series/database/seeders/DatabaseSeeder.php
+++ series/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class phpDatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
